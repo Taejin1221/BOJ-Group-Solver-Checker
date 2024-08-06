b570842e505ae35cf4fa3c62dca661438d4c81a3
taejin1221 <taejin7824@gmail.com>
2024-08-07 02:20:55 +0900

    Feat: Tag별 안푼문제 확인 시 난이도 정보 제공
    + .gitignore에 test.ipynb 추적 안하도록 수정
    + README.md 수정
    + URL "www.acmicpc.net/problem/<id>"로 수정
    + BOJ_Crawler module에 sort_problems_by_level 함수 추
    
    Related to: #1

--- BOJ_Crawler.py
+++ BOJ_Crawler.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2719.1708 Safari/537.36"}
+TIER_LIST = ['Unrated/NotRatable', 'Bronze V', 'Bronze VI', 'Bronze III', 'Bronze II', 'Bronze I', 'Silver V', 'Silver VI', 'Silver III', 'Silver II', 'Silver I', 'Gold V', 'Gold VI', 'Gold III', 'Gold II', 'Gold I', 'Platinum V', 'Platinum VI', 'Platinum III', 'Platinum II', 'Platinum I', 'Diamond V', 'Diamond VI', 'Diamond III', 'Diamond II', 'Diamond I', 'Ruby V', 'Ruby VI', 'Ruby III', 'Ruby II', 'Ruby I']
 
 def get_solved_problem(user):
     url = f'https://www.acmicpc.net/user/{user}'
@@ -41,6 +42,21 @@ def get_tag_problems(tag: int):
         return {"success": True, "problems": tag_problems}
     else:
         return {"success": False, "statusCode": response.status_code}
+
+def sort_problems_by_level(problems: list[int]):
+    url = f"https://solved.ac/api/v3/problem/lookup?problemIds={','.join(list(map(str, problems)))}"
+    res = requests.get(url)
+
+    if (res.status_code == 200):
+        problems = [{ 'id':  prob['problemId'], 'level': prob['level'], 'url': f"boj.kr/{prob['problemId']}"} for prob in res.json()]
+        problems.sort(key=lambda x: x['level'])
+        for prob in problems:
+            prob['level'] = TIER_LIST[prob['level']]
+
+        return {"success": True, "problems": problems}
+
+    else:
+        return {"success": False, "statusCode": res.status_code}
 
 if (__name__ == '__main__'):
     print(get_solved_problem('jin99'))
