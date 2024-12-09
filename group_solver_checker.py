49d33fcb95357d37444c1265487534d4788b7dfb
taejin1221 <taejin7824@gmail.com>
2024-12-10 01:02:53 +0900

    Refactor: group_solver_checker 모듈화
    Related to: #6

--- group_solver_checker.py
+++ group_solver_checker.py
@@ -1,80 +1,29 @@
-import json, sys
+from boj_crawler import get_problems_by_tag, sort_problems_by_level
 
-from boj_crawler import get_solved_problem, get_problems_by_tag, sort_problems_by_level
+def get_solved_user(solved_problem_list: list[dict], problem: int) -> list:
+    solved_handles = []
+    for solved_info in solved_problem_list:
+        if problem in solved_info["problems"]:
+            solved_handles.append(solved_info["handle"])
 
-if (sys.argv[1] == '-s' or sys.argv[1] == '--solved'): # Group member들이 푼 문제 list가 주어졌을 때
-    with open(sys.argv[2]) as f:
-        solved_problem_list = json.load(f)
+    return solved_handles
 
-elif (sys.argv[1] == '-h' or sys.argv[1] == '--handle'): # 푼 문제 list가 아닌 handle만 주어졌을 때
-    with open(sys.argv[2]) as f:
-        group_members_list = json.load(f)['handles']
-
-    file_list = []
-    for member in group_members_list:
-        res = get_solved_problem(member)
-        if (res['success']):
-            file_list.append(res['fileName'])
-            print(res['fileName'])
-        else:
-            print(f"Error: {member}, {res['statusCode']}")
-            sys.exit(1)
-
-    solved_problem_list = []
-    for file in file_list:
-        with open(file) as f:
-            solved_problem_list.append(json.load(f))
-
-else:
-    print(f"usage: python3 {sys.argv[0]} [-s, -h] FILE\n")
-    print("options:")
-    print("  -s, --solved: Give solved problem list of all members.")
-    print("  -h, --handle: Give all member's handles.")
-
-    sys.exit(1)
-
-oper = input('Do you input manually or choose tag([m/t])? ')
-if (oper == 'm'):
-    problem = input("Input problem [int/q]: ")
-    while (problem != 'q'):
-        try:
-            problem = int(problem)
-
-            solved_handle = []
-            for solved_list in solved_problem_list:
-                if (problem in solved_list['problems']):
-                    solved_handle.append(solved_list['handle'])
-
-            if (len(solved_handle) > 0):
-                print(f"Handle: {', '.join(solved_handle)}")
-            else:
-                print(f"No, https://www.acmicpc.net/problem/{problem}")
-        except ValueError as e:
-            print('Error: You can input only "int" and "q"')
-        print('')
-
-        problem = input("Input problem number: ")
-
-    print("quit")
-elif (oper == 't'):
-    tag = input('Input tag: ')
+def get_unsolved_problems_by_tag(solved_problem_list: list[dict], tag: int) -> list[dict] | dict:
     res = get_problems_by_tag(tag)
-    if (res['success']):
-        sort_res = sort_problems_by_level(res['problems'])
-        if (sort_res['success']):
-            problems = sort_res['problems']
-        else:
-            print(f"Error: {tag}, {res['statusCode']}")
-            sys.exit(1)
-    else:
-        print(f"Error: {tag}, {res['statusCode']}")
-        sys.exit(1)
+    if not res["success"]:
+        return res
 
+    sort_res = sort_problems_by_level(res["problems"])
+    if not sort_res["success"]:
+        return sort_res
+
+    problems = sort_res["problems"]
+
+    result = []
     for problem in problems:
-        solved_handle = []
-        for solved_list in solved_problem_list:
-            if (problem['id'] in solved_list['problems']):
-                solved_handle.append(solved_list['handle'])
+        solved_handle = get_solved_user(solved_problem_list, problem["id"])
 
-        if (len(solved_handle) == 0):
-            print(f"{problem['level']:12}, 'https://www.acmicpc.net/problem/{problem['id']}'")
+        if len(solved_handle) == 0:
+            result.append({"level": problem["level"], "URL": f"https://www.acmicpc.net/problem/{problem['id']}"})
+
+    return result
